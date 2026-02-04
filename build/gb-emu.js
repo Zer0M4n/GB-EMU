6b203f5988fdb1e4c182552aec4b7600c89c88e0
Zer0M4n <l22211575@tectijuana.edu.mx>
2026-02-03 17:29:23 -0800

    Add new CPU instruction prototypes to cpu.h
    - Introduced prototypes for various logical and arithmetic operations including XOR, ADC, and rotation instructions.
    - Added support for restart instructions and additional load operations.
    - Enhanced the CPU interface for better instruction handling.

--- build/gb-emu.js
+++ build/gb-emu.js
@@ -27,7 +27,7 @@ var ENVIRONMENT_IS_SHELL = !ENVIRONMENT_IS_WEB && !ENVIRONMENT_IS_NODE && !ENVIR
 
 // --pre-jses are emitted after the Module integration code, so that they can
 // refer to Module (if they choose; they can also define Module)
-// include: /tmp/tmpcc1l1hfi.js
+// include: /tmp/tmplrxmnngt.js
 
   Module['expectedDataFileDownloads'] ??= 0;
   Module['expectedDataFileDownloads']++;
@@ -208,14 +208,14 @@ Module['FS_createPath']("/", "roms", true, true);
 
   })();
 
-// end include: /tmp/tmpcc1l1hfi.js
-// include: /tmp/tmpv5g77wfi.js
+// end include: /tmp/tmplrxmnngt.js
+// include: /tmp/tmpb4ygjur9.js
 
     // All the pre-js content up to here must remain later on, we need to run
     // it.
     if ((typeof ENVIRONMENT_IS_WASM_WORKER != 'undefined' && ENVIRONMENT_IS_WASM_WORKER) || (typeof ENVIRONMENT_IS_PTHREAD != 'undefined' && ENVIRONMENT_IS_PTHREAD) || (typeof ENVIRONMENT_IS_AUDIO_WORKLET != 'undefined' && ENVIRONMENT_IS_AUDIO_WORKLET)) Module['preRun'] = [];
     var necessaryPreJSTasks = Module['preRun'].slice();
-  // end include: /tmp/tmpv5g77wfi.js
+  // end include: /tmp/tmpb4ygjur9.js
 // include: /home/developer/emsdk/upstream/emscripten/src/emrun_prejs.js
 /**
  * @license
@@ -238,13 +238,13 @@ if (typeof window == 'object') {
   }
 }
 // end include: /home/developer/emsdk/upstream/emscripten/src/emrun_prejs.js
-// include: /tmp/tmpod0u26_q.js
+// include: /tmp/tmpn5rfaetd.js
 
     if (!Module['preRun']) throw 'Module.preRun should exist because file support used it; did a pre-js delete it?';
     necessaryPreJSTasks.forEach((task) => {
       if (Module['preRun'].indexOf(task) < 0) throw 'All preRun tasks that exist before user pre-js code should remain after; did you replace Module or modify Module.preRun?';
     });
-  // end include: /tmp/tmpod0u26_q.js
+  // end include: /tmp/tmpn5rfaetd.js
 
 
 var arguments_ = [];
@@ -5362,7 +5362,7 @@ function checkIncomingModuleAPI() {
   ignoredModuleProp('fetchSettings');
 }
 var ASM_CONSTS = {
-  84292: () => { drawCanvas(); }
+  84356: () => { drawCanvas(); }
 };
 
 // Imports from the Wasm binary.
